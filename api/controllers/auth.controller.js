c02a0db547a37a025ae2abdfe26abe56fb7530c0
sarniraula <sar.niraula162@gmail.com>
2024-01-23 22:43:08 -0500

    create a signup api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+
+  const newUser = new User({
+    username,          //this is same as saying username: username, email: email, password: password
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose
@@ -15,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on Port 3000!!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
