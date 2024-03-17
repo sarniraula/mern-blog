06937ee608d1dd5596ee9cf719f94e9ca3b789fe
sarniraula <sar.niraula162@gmail.com>
2024-03-17 15:18:08 -0400

    Sidebar of the dashboard.

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { Sidebar } from 'flowbite-react'
+import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+
+export default function DashSidebar() {
+    const locaton = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(locaton.search);
+        const tabFromUrl = urlParams.get('tab');
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [locaton.search])
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to='/dashboard?tab=profile'>
+                        <Sidebar.Item 
+                            active={tab === 'profile'} 
+                            icon={HiUser} 
+                            label={'User'} 
+                            labelColor='dark'
+                        >
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
 
 export default function Dashboard() {
+  const locaton = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(locaton.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [locaton.search])
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        <DashSidebar />
+      </div>
+      { tab === 'profile' && <DashProfile /> }
     </div>
   )
 }
