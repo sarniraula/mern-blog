3d2dc6f84e7a65d61290066b039fd5bb6ae181f9
sarniraula <sar.niraula162@gmail.com>
2024-06-22 23:28:50 -0400

    Added signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -12,6 +12,7 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signoutSuccess,
 } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
@@ -133,6 +134,22 @@ export default function DashProfile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = res.json();
+      if(!res.ok) {
+        console.log(data.message)
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -222,7 +239,7 @@ export default function DashProfile() {
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>
           Delete Account
         </span>
-        <span className='cursor-pointer'>
+        <span onClick={handleSignout} className='cursor-pointer'>
           Sign Out
         </span>
       </div>
