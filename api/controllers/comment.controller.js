869fb2796de2fdd148cfe573fa436de06f11bccf
sarniraula <sar.niraula162@gmail.com>
2024-10-03 21:15:34 -0400

    Build the backend for comment section.

---
+++ api/controllers/comment.controller.js
@@ -0,0 +1,21 @@
+import { errorHandler } from "../utils/error.js";
+
+export const createComment = async (req, res, next) => {
+    try {
+        const { content, postId, userId } = req.body;
+        
+        if (userId !== req.userId) {
+            return next(errorHandler(403, 'You are not allowed to create this comment'))
+        }
+
+        const newComment = new Comment({
+            content,
+            postId,
+            userId
+        });
+        await newComment.save();
+        res.status(201).json(newComment);
+    } catch(error) {
+        next(error);
+    }
+} 
